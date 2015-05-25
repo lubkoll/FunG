05add53d93f0084e23543d7e20d472dfd094f913
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-05-26 00:52:06 +0200

    scale test

---
+++ test/MathematicalOperations/scale_unittest.cpp
@@ -0,0 +1,35 @@
+#include "../../src/CMath/pow.hh"
+#include "../../src/generate.hh"
+#include "../../src/finalize.hh"
+
+#include <gtest/gtest.h>
+
+TEST(ScaleTest,D0)
+{
+  using RFFGen::CMath::Pow;
+  auto fun = 2*Pow<3,1>(2.);
+  EXPECT_DOUBLE_EQ( fun.d0() , 16. );
+}
+
+TEST(ScaleTest,D1)
+{
+  using RFFGen::CMath::Pow;
+  auto fun = RFFGen::finalize_scalar ( 2*Pow<3,1>(2.) );
+  EXPECT_DOUBLE_EQ( fun.d1() , 24. );
+}
+
+TEST(ScaleTest,D2)
+{
+  using RFFGen::CMath::Pow;
+  auto fun = RFFGen::finalize_scalar( 2*Pow<3,1>(2.) );
+  EXPECT_DOUBLE_EQ( fun.d2() , 24. );
+}
+
+TEST(ScaleTest,D3)
+{
+  using RFFGen::CMath::Pow;
+  auto fun = RFFGen::finalize_scalar( 2*Pow<3,1>(2.) );
+  EXPECT_DOUBLE_EQ( fun.d3() , 12. );
+}
+
+
