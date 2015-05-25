8a719d76b9adfd9221171b63303d50c20da7dbf0
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-05-26 00:16:10 +0200

    sum test

--- test/MathematicalOperations/sum_unittest.cpp
+++ test/MathematicalOperations/sum_unittest.cpp
@@ -14,7 +14,7 @@ TEST(SumTest,D1)
 {
   using RFFGen::CMath::Pow;
   auto fun = Pow<3,1>(2.) + Pow<3,2>(1.);
-  EXPECT_DOUBLE_EQ( fun.d1<0>(1) , 13.5. );
+  EXPECT_DOUBLE_EQ( fun.d1<0>(1) , 13.5 );
 }
 
 TEST(SumTest,D2)
