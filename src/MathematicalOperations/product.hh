0d1ff2402006ed6a7d0e4b27e0a21e8225680c91
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-05-26 01:05:11 +0200

    removed operator()

--- src/MathematicalOperations/product.hh
+++ src/MathematicalOperations/product.hh
@@ -114,21 +114,6 @@ namespace RFFGen
         g.template updateVariable<index>(x);
       }
 
-
-      /// Function value. Convenient access to d0().
-      const auto& operator()() const noexcept
-      {
-        return d0();
-      }
-
-      /// Function value. Convenient access to d0() with prior call to update(x).
-      template <class Arg>
-      const auto& operator()(const Arg& x) const noexcept
-      {
-        update(x);
-        return d0();
-      }
-
       /// Function value.
       auto d0() const noexcept
       {
