049e02c9a8ecbf114b2ec440b30eaf273c9c221e
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-05-26 19:48:41 +0200

    fix finalize.hh

--- RFFGen/finalize.hh
+++ RFFGen/finalize.hh
@@ -275,7 +275,7 @@ namespace RFFGen
    * Adds the definition of possibly undefined vanishing higher order derivatives.
    */
   template <class F, bool arithmeticArgument = false>
-  using Finalize = Detail::FinalizeImpl< F , arithmeticArgument , Checks::hasMoreThanOneVariable<F>() >;
+  using Finalize = Detail::FinalizeImpl< F , arithmeticArgument , Checks::hasVariable<F>() >;
 
   /**
    * \brief Finish function definition.
