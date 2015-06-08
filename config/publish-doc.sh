58027d6081a84a0e1c5afbf449fc28ab7c027e60
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-06-08 16:56:01 +0200

    + docs

---
+++ config/publish-doc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+REPO_PATH=https://github.com/lubkoll/RFFGen
+HTML_PATH=build/doc/html
+COMMIT_USER="Documentation Generator"
+COMMIT_EMAIL="lars.lubkoll@posteo.de"
+CHANGESET=$(git rev-parse --verify HEAD)
+
+# Get a clean version of the HTML documentation repo.
+rm -rf ${HTML_PATH}
+mkdir -p ${HTML_PATH}
+git clone -b gh-pages "${REPO_PATH}" --single-branch ${HTML_PATH}
+
+# rm all the files through git to prevent stale files.
+cd ${HTML_PATH}
+git rm -rf .
+cd -
+
+# Generate the HTML documentation.
+doxygen
+
+# Create and commit the documentation repo.
+cd ${HTML_PATH}
+git add .
+git config user.name "${COMMIT_USER}"
+git config user.email "${COMMIT_EMAIL}"
+git commit -m "Automated documentation build for changeset ${CHANGESET}."
+git push origin gh-pages
+cd -
