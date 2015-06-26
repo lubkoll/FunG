b768d32e3f41588135754e62b1478406d0f9f017
Lars Lubkoll <lars.lubkoll@posteo.de>
2015-06-26 13:23:10 +0200

    docs

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -2,9 +2,8 @@ var searchData=
 [
   ['euclideannorm',['EuclideanNorm',['../group__LinearAlgebraGroup.html#gaee2184589f2dddd26f00bd695ebcd577',1,'RFFGen::LinearAlgebra']]],
   ['examples',['Examples',['../group__Examples.html',1,'']]],
-  ['exp',['Exp',['../structRFFGen_1_1CMath_1_1Exp.html#ac87aaf4564d1e254229f8b8d95798fb6',1,'RFFGen::CMath::Exp']]],
   ['exp',['Exp',['../structRFFGen_1_1CMath_1_1Exp.html',1,'RFFGen::CMath']]],
-  ['exp2',['Exp2',['../structRFFGen_1_1CMath_1_1Exp2.html',1,'RFFGen::CMath']]],
+  ['exp',['Exp',['../structRFFGen_1_1CMath_1_1Exp.html#ac87aaf4564d1e254229f8b8d95798fb6',1,'RFFGen::CMath::Exp']]],
   ['exp2',['Exp2',['../structRFFGen_1_1CMath_1_1Exp2.html#a4df27b5cd85288709f76a87efc4a5a87',1,'RFFGen::CMath::Exp2']]],
-  ['extractdimension',['ExtractDimension',['../structRFFGen_1_1LinearAlgebra_1_1ExtractDimension.html',1,'RFFGen::LinearAlgebra']]]
+  ['exp2',['Exp2',['../structRFFGen_1_1CMath_1_1Exp2.html',1,'RFFGen::CMath']]]
 ];
