06ba2ce8f59db10a32c4bfa3f923b9dc14b2f935
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-23 18:47:11 +0100

    closes #3 Fix wrong naming of right Cauchy-Green strain tensor and add implementation of left Cauchy-Green strain tensor

--- fung/linear_algebra/strain_tensor.hh
+++ fung/linear_algebra/strain_tensor.hh
@@ -1,179 +1,171 @@
-// Copyright (C) 2015 by Lars Lubkoll. All rights reserved.
-// Released under the terms of the GNU General Public License version 3 or later.
-
-#ifndef FUNG_LINEAR_ALGEBRA_STRAIN_TENSOR_HH
-#define FUNG_LINEAR_ALGEBRA_STRAIN_TENSOR_HH
+#pragma once
 
 #include "transpose.hh"
-#include "fung/mathematical_operations/sum.hh"
-#include "fung/util/chainer.hh"
-#include "fung/util/add_transposed_matrix.hh"
+#include <fung/mathematical_operations/sum.hh>
+#include <fung/util/chainer.hh>
+#include <fung/util/add_transposed_matrix.hh>
 
 namespace FunG
 {
-  /// @cond
-  namespace Concepts { template <class> struct SquareMatrixConceptCheck; }
-  ///@endcond
+    /// @cond
+    namespace Concepts { template <class> struct SquareMatrixConceptCheck; }
+    ///@endcond
 
-  namespace LinearAlgebra
-  {
-    /**
-     * @ingroup LinearAlgebraGroup
-     * @brief Left Cauchy-Green strain tensor \f$ F^T F \f$ for a symmetric matrix \f$ F \f$.
-     *
-     * This class is used for nonlinear material models based on the deformation gradient \f$\nabla\varphi\f$, which takes the role of \f$F\f$.
-     * Caches both \f$ F^T \f$ and \f$ F^T F \f$.
+    /** @addtogroup LinearAlgebraGroup
+     * @{
      */
-    template <class Matrix, class = Concepts::SquareMatrixConceptCheck<Matrix> >
-    class LeftCauchyGreenStrainTensor :
-        public Chainer< LeftCauchyGreenStrainTensor<Matrix , Concepts::SquareMatrixConceptCheck<Matrix> > >
+    namespace LinearAlgebra
     {
-    public:
-      LeftCauchyGreenStrainTensor() = default;
-      /**
-       * @brief Constructor.
-       * @param F point of evaluation.
-       */
-      explicit LeftCauchyGreenStrainTensor(Matrix const& F) { update(F); }
-
-      /// Reset point of evaluation.
-      void update(Matrix const& F)
-      {
-        if( !initialized )
+        /**
+         * @brief Right Cauchy-Green strain tensor \f$ F^T F \f$ for a symmetric matrix \f$ F \f$.
+         *
+         * Used in nonlinear material models based on the deformation gradient \f$\nabla\varphi\f$, which takes the role of \f$F\f$.
+         */
+        template <class Matrix, class = Concepts::SquareMatrixConceptCheck<Matrix> >
+        class RightCauchyGreenStrainTensor :
+                public Chainer< RightCauchyGreenStrainTensor<Matrix , Concepts::SquareMatrixConceptCheck<Matrix> > >
         {
-          new(&FT) Matrix{Detail::transpose(F)};
-          new(&FTF) Matrix{FT*F};
-          initialized = true;
-        }
-        else
+        public:
+            RightCauchyGreenStrainTensor() = default;
+            /**
+             * @brief Constructor.
+             * @param F point of evaluation.
+             */
+            explicit RightCauchyGreenStrainTensor(const Matrix& F)
+            { update(F); }
+
+            /// Reset point of evaluation.
+            void update(const Matrix& F)
+            {
+                FT = Detail::transpose(F);
+                FTF = FT * F;
+            }
+
+            /// Function value \f$ F^T * F \f$.
+            const Matrix& d0() const noexcept
+            {
+                return FTF;
+            }
+
+            /// First directional derivative \f$ F^T dF_1 + dF_1^T F \f$.
+            Matrix d1(const Matrix& dF1) const
+            {
+                Matrix FTdF1 = FT * dF1;
+                return addTransposed(FTdF1);
+            }
+
+            /// Second directional derivative \f$ dF_2^T dF_1 + dF_1^T dF_2 \f$.
+            Matrix d2(const Matrix& dF1, const Matrix& dF2) const
+            {
+                Matrix dF2TdF1 = Detail::transpose(dF2) * dF1;
+                return addTransposed(dF2TdF1);
+            }
+
+        private:
+            Matrix FT, FTF;
+        };
+
+
+        /**
+         * @brief Left Cauchy-Green strain tensor \f$ F^T F \f$ for a symmetric matrix \f$ F \f$.
+         *
+         * Used in nonlinear material models based on the deformation gradient \f$\nabla\varphi\f$, which takes the role of \f$F\f$.
+         */
+        template <class Matrix, class = Concepts::SquareMatrixConceptCheck<Matrix> >
+        class LeftCauchyGreenStrainTensor :
+                public Chainer< LeftCauchyGreenStrainTensor<Matrix , Concepts::SquareMatrixConceptCheck<Matrix> > >
         {
-          FT = Detail::transpose(F);
-          FTF = FT * F;
+        public:
+            LeftCauchyGreenStrainTensor() = default;
+            /**
+             * @brief Constructor.
+             * @param F point of evaluation.
+             */
+            explicit LeftCauchyGreenStrainTensor(const Matrix& F)
+            { update(F); }
+
+            /// Reset point of evaluation.
+            void update(const Matrix& F)
+            {
+                FT = Detail::transpose(F);
+                FFT = F * FT;
+            }
+
+            /// Function value \f$ F^T * F \f$.
+            const Matrix& d0() const noexcept
+            {
+                return FFT;
+            }
+
+            /// First directional derivative \f$ F^T dF_1 + dF_1^T F \f$.
+            Matrix d1(const Matrix& dF1) const
+            {
+                Matrix FTdF1 = dF1 * FT;
+                return addTransposed(FTdF1);
+            }
+
+            /// Second directional derivative \f$ dF_2^T dF_1 + dF_1^T dF_2 \f$.
+            Matrix d2(const Matrix& dF1, const Matrix& dF2) const
+            {
+                Matrix dF1dF2T = dF1 * Detail::transpose(dF2);
+                return addTransposed(dF1dF2T);
+            }
+
+        private:
+            Matrix FT, FFT;
+        };
+
+
+        /**
+         * \brief Generate the right Cauchy-Green strain tensor \f$A*A^T\f$.
+         * \param A matrix
+         * \return RightCauchyGreenStrainTensor<Matrix>(A)
+         */
+        template <class Matrix,
+                  std::enable_if_t<!Checks::isFunction<Matrix>()>* = nullptr>
+        auto strainTensor(const Matrix& A)
+        {
+            return RightCauchyGreenStrainTensor<Matrix>{A};
         }
-      }
-
-      /// Function value \f$ F^T * F \f$.
-      Matrix const& d0() const noexcept
-      {
-        return FTF;
-      }
-
-      /// First directional derivative \f$ F^T dF_1 + dF_1^T F \f$.
-      Matrix d1(Matrix const& dF1) const
-      {
-        Matrix FTdF1 = FT * dF1;
-        return addTransposed(FTdF1);
-      }
-
-      /// Second directional derivative \f$ dF_2^T dF_1 + dF_1^T dF_2 \f$.
-      Matrix d2(Matrix const& dF1, Matrix const& dF2) const
-      {
-        Matrix dF2TdF1 = Detail::transpose(dF2) * dF1;
-        return addTransposed(dF2TdF1);
-      }
-
-    private:
-      Matrix FT, FTF;
-      bool initialized = false;
-    };
 
 
-    /**
-     * \brief Generate the left Cauchy-Green strain tensor \f$A^T*A\f$.
-     * \param A matrix
-     * \return LeftCauchyGreenStrainTensor<Matrix>(A)
-     */
-    template <class Matrix,
-              std::enable_if_t<!Checks::isFunction<Matrix>()>* = nullptr>
-    auto strainTensor(const Matrix& A)
-    {
-      return LeftCauchyGreenStrainTensor<Matrix>{A};
+        /**
+         * \brief Generate the right Cauchy-Green strain tensor \f$f*f^T\f$, where \f$f:\cdot\mapsto\mathbb{R}^{n,n} \f$.
+         * \param f function object mapping into a space of square matrices
+         * \return RightCauchyGreenStrainTensor< decay_t<decltype(f())> >(f())( f )
+         */
+        template <class F,
+                  std::enable_if_t<Checks::isFunction<F>()>* = nullptr>
+        auto strainTensor(const F& f)
+        {
+            return RightCauchyGreenStrainTensor< decay_t<decltype(f())> >{ f() }( f );
+        }
+
+
+        /**
+         * \brief Generate the left Cauchy-Green strain tensor \f$A^T*A\f$.
+         * \param A matrix
+         * \return LeftCauchyGreenStrainTensor<Matrix>(A)
+         */
+        template <class Matrix,
+                  std::enable_if_t<!Checks::isFunction<Matrix>()>* = nullptr>
+        auto leftStrainTensor(const Matrix& A)
+        {
+            return LeftCauchyGreenStrainTensor<Matrix>{A};
+        }
+
+
+        /**
+         * \brief Generate the left Cauchy-Green strain tensor \f$f^T*f\f$, where \f$f:\cdot\mapsto\mathbb{R}^{n,n} \f$.
+         * \param f function object mapping into a space of square matrices
+         * \return LeftCauchyGreenStrainTensor< decay_t<decltype(f())> >(f())( f )
+         */
+        template <class F,
+                  std::enable_if_t<Checks::isFunction<F>()>* = nullptr>
+        auto leftStrainTensor(const F& f)
+        {
+            return LeftCauchyGreenStrainTensor< decay_t<decltype(f())> >{ f() }( f );
+        }
     }
-
-
-    /**
-     * \brief Generate the left Cauchy-Green strain tensor \f$f^Tf\f$, where \f$f:\cdot\mapsto\mathbb{R}^{n,n} \f$.
-     * \param f function object mapping into a space of square matrices
-     * \return LeftCauchyGreenStrainTensor< std::decay_t<decltype(f())> >(f())( f )
-     */
-    template <class F,
-              std::enable_if_t<Checks::isFunction<F>()>* = nullptr>
-    auto strainTensor(const F& f)
-    {
-      return LeftCauchyGreenStrainTensor< decay_t<decltype(f())> >{ f() }( f );
-    }
-
-//    /**
-//     * \ingroup LinearAlgebraGroup
-//     * \brief Linearized strain tensor \f$ \frac{1}{2}\left(F^T+F\right) \f$.
-//     *
-//     * This class is used for linear material models based on the displacement gradient \f$\nabla u\f$, which takes the role of \f$F\f$.
-//     * Caches the function value \f$ \frac{1}{2}\left(F^T+F\right) \f$.
-//     */
-//    template <class Matrix, class = Concepts::SquareMatrixConceptCheck<Matrix> >
-//    class LinearizedStrainTensor :
-//        public Base ,
-//        public Chainer< LinearizedStrainTensor< Matrix , Concepts::SquareMatrixConceptCheck<Matrix> > >
-//    {
-//    public:
-//      /**
-//       * @brief Constructor.
-//       * @param F point of evaluation
-//       */
-//      explicit LinearizedStrainTensor(const Matrix& F) { update(F); }
-
-//      /// Reset point of evaluation.
-//      void update(Matrix const& F)
-//      {
-//        d0Result = F + Detail::transpose(F);
-//        d0Result *= 0.5;
-//      }
-
-//      /// Function value \f$ \frac{1}{2}\left(F^T+F\right) \f$.
-//      Matrix const& d0() const
-//      {
-//        return d0Result;
-//      }
-
-//      /// First directional derivative \f$ \frac{1}{2}\left(dF^T+dF\right) \f$.
-//      template <int>
-//      Matrix d1(const Matrix& dF) const
-//      {
-//        return 0.5*(dF + Detail::transpose(dF));
-//      }
-
-//    private:
-//      Matrix d0Result = Matrix{0.};
-//    };
-
-//    /**
-//     * \ingroup LinearAlgebraGroup
-//     * \brief Model of the geometric nonlinearity in elasticity theory.
-//     * Implemented as template-alias to CauchyGreenStrainTensor.
-//     */
-//    template <class Matrix> using GeometricNonlinearity = LeftCauchyGreenStrainTensor<Matrix>;
-
-//    /**
-//     * \ingroup LinearAlgebraGroup
-//     * \brief Strain tensor \f$ \frac{1}{2}\left(F^T+F + F^T F\right) \f$.
-//     *
-//     * This class is used for nonlinear material models based on the displacement gradient \f$\nabla u\f$, which takes the role of \f$F\f$.
-//     * Implemented as Sum<LinearizedStrainTensor,GeometricNonlinearity>.
-//     */
-//    template <class Matrix>
-//    class StrainTensor :
-//        public MathematicalOperations::Sum< LinearizedStrainTensor<Matrix> , GeometricNonlinearity<Matrix> > ,
-//        public Chainer< StrainTensor<Matrix> >
-//    {
-//      using Base = MathematicalOperations::Sum< LinearizedStrainTensor<Matrix> , GeometricNonlinearity<Matrix> >;
-//    public:
-//      /**
-//       * @brief Constructor.
-//       * @param F point of evaluation
-//       */
-//      explicit StrainTensor(const Matrix& F) : Base(F) {}
-//    };
-  }
+    /** @} */
 }
-
-#endif // FUNG_LINEAR_ALGEBRA_STRAIN_TENSOR_HH
