eb105868c07f452c4748209418cfacf84cf4af4e
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-12-06 23:01:29 +0100

    Minor changes

--- fung/fung.hh
+++ fung/fung.hh
@@ -1,18 +1,12 @@
-// Copyright (C) 2015 by Lars Lubkoll. All rights reserved.
-// Released under the terms of the GNU General Public License version 3 or later.
+#pragma once
 
-#ifndef FUNG_HH
-#define FUNG_HH
-
-#include "fung/concept_check.hh"
-#include "fung/constant.hh"
-#include "fung/finalize.hh"
-#include "fung/generate.hh"
-#include "fung/identity.hh"
-#include "fung/linear_algebra.hh"
-#include "fung/math.hh"
-#include "fung/operations.hh"
-#include "fung/variable.hh"
-#include "fung/util/add_missing_operators.hh"
-
-#endif // FUNG_HH
+#include "concept_check.hh"
+#include "constant.hh"
+#include "finalize.hh"
+#include "generate.hh"
+#include "identity.hh"
+#include "linear_algebra.hh"
+#include "math.hh"
+#include "operations.hh"
+#include "variable.hh"
+#include "util/add_missing_operators.hh"
