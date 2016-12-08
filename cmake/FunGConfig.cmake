a3c1323f2501b9eb72877b461e8c296a7bc6167c
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-12-08 22:53:17 +0100

    Properly install FunG with cmake install functionality and add additional install target 'doc'

---
+++ cmake/FunGConfig.cmake
@@ -0,0 +1,6 @@
+# - Config file for the FunG package 
+set(FunG_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
+set(FunG_CXX_FLAGS "-std=c++14")
+set(Spacy_LIBRARIES "")
+set(Spacy_LIBRARY_DIR "")
+set(Spacy_LIBS "")
