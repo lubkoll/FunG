8ae082c63180eef5b909b4860cdb00d76d5a5fcc
Travis CI <lars.lubkoll@posteo.de>
2020-06-20 10:55:15 +0000

    Update documentation for commit: 7b73542b1d65845b755b7ebd8e3707ea8b44f176

--- doc/html/search/files_3.js
+++ doc/html/search/files_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['erf_2ehh',['erf.hh',['../erf_8hh.html',1,'']]],
   ['evaluate_5fif_5fpresent_2ehh',['evaluate_if_present.hh',['../evaluate__if__present_8hh.html',1,'']]],
   ['exceptions_2ehh',['exceptions.hh',['../exceptions_8hh.html',1,'']]],
   ['exp_2ehh',['exp.hh',['../exp_8hh.html',1,'']]],
--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['erf',['Erf',['../structFunG_1_1Erf.html#a98e6b3c8281468c2e548862b1b6cb3b2',1,'FunG::Erf::Erf()'],['../group__CMathGroup.html#gaa0d608a0906520bd48b7e0036e560afa',1,'FunG::erf()']]],
   ['exp',['Exp',['../structFunG_1_1Exp.html#a2abec31ebf5f8ee4877fb1c05d2fdad2',1,'FunG::Exp::Exp()'],['../group__CMathGroup.html#ga00135c8521411f13813f9b972ea4e231',1,'FunG::exp()']]],
   ['exp2',['Exp2',['../structFunG_1_1Exp2.html#aa5e03d1e3a39a1c2151cdfe092461060',1,'FunG::Exp2::Exp2()'],['../group__CMathGroup.html#ga7724faf7db6e9583ac91df322c26ecf5',1,'FunG::exp2()']]]
 ];
